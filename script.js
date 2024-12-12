90420f7acc34630823f4795870dc1125d0e86a02
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-12-11 20:46:36 -0500

    Snake movement and food regeneration

--- script.js
+++ script.js
@@ -2,18 +2,44 @@ const playBoard = document.querySelector(".play-board");
 
 let foodX, foodY;
 let snakeX = 5, snakeY = 20;
+let velocityX = 0, velocityY = 0;
 
-const changFoodPosition = () => {
+const changeFoodPosition = () => {
     foodX = Math.floor(Math.random()*30)+1;
     foodY = Math.floor(Math.random()*30)+1;
-    console.log(foodX);
+}
+
+const changeDirection = (e) => {
+    if (e.key === "ArrowUp") {
+        velocityX = 0;
+        velocityY = -1;
+    } else if(e.key === "ArrowDown"){
+        velocityX = 0;
+        velocityY = 1;
+    } else if(e.key === "ArrowLeft"){
+        velocityX = -1;
+        velocityY = 0;
+    } else if(e.key === "ArrowRight"){
+        velocityX = 1;
+        velocityY = 0;
+    }
 }
 
 const initGame =() => {
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+    if (snakeX === foodX && foodX && snakeY === foodY)  {
+        changeFoodPosition();
+    } 
+
+    snakeX += velocityX;
+    snakeY += velocityY;
+
     htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
     playBoard.innerHTML = htmlMarkup;
 }
 
-changFoodPosition();
-initGame();
+changeFoodPosition();
+setInterval(initGame, 125);
+document.addEventListener("keydown", changeDirection);
+
